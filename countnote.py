97a1087651083b936b62de00c974b77b02856ab1
Sasikala-codingal <sasikalaramasamy.codingal@gmail.com>
2025-12-18 14:07:22 +0530

    Add files via upload

---
+++ countnote.py
@@ -0,0 +1,8 @@
+print("Program to find the number of notes to Withdraw from ATM")
+amount=int(input("Enter the amount:"))
+note_100=amount/100
+note_50=(amount%100)//50
+note_10=((amount%100)%50)//10
+print("Number of 100 notes:",int(note_100)) 
+print("Number of 50 notes:",int(note_50))
+print("Number of 10 notes:",int(note_10))
