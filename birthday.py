0d1753e310d766edc702815b41bedd11dc5bb677
Sasikala-codingal <sasikalaramasamy.codingal@gmail.com>
2025-12-28 18:51:11 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,11 @@
+print("Birthdays of my friends are : \n")
+a="12/12/2008"
+b="14/09/2005"
+c="23/06/2007"
+d="12/06/1997"
+e="06/07/2006"
+print("Joanna : ",a)
+print("Suresh : ",b)
+print("Anitha : ",c)
+print("Aishwarya: ",d)
+print("Nanda : ",e)
---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to Coding")
+
+print(10)
+print("NEW LINE \n")
+print("This vlue of 2+2=",2+2)
+print("THank you" , end="*")
