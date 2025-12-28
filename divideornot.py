e4e8c5ec64d07102e44554447432489b34452e4f
Sasikala-codingal <sasikalaramasamy.codingal@gmail.com>
2025-12-28 21:57:13 +0530

    Add files via upload

---
+++ divideornot.py
@@ -0,0 +1,7 @@
+print("Enter the numerator: ")
+numerator = float(input())
+print("Enter the denominator: ")
+denominator = float(input())
+if denominator != 0:
+    result = numerator / denominator
+    print("The result is:", result)
---
+++ mean.py
@@ -0,0 +1,8 @@
+mean1=38
+n=40
+wn=56
+cn=36
+sum=mean1*n
+sum2=sum-wn+cn
+mean2=sum2/n
+print("The new mean is ", mean2)
