670a0f780d049dfe62db640a5b2347a53765a124
Sasikala-codingal <sasikalaramasamy.codingal@gmail.com>
2025-12-30 18:46:57 +0530

    Add files via upload

---
+++ ACP-turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("pink")
+
+pen = turtle.Turtle()
+pen.speed(0)
+
+pen.fillcolor("red")
+pen.begin_fill()
+
+pen.circle(100)
+
+
+pen.end_fill()
+pen.hideturtle()
+
+
+turtle.done()
