38de4a8df0dfc3707e35dcb0eb2a4394cbd5b554
Sasikala-codingal <sasikalaramasamy.codingal@gmail.com>
2026-03-02 00:33:58 +0530

    Add files via upload

--- countrycode.py
+++ countrycode.py
@@ -1,3 +1,11 @@
-s1={"c","o","d"}
-s2={"C","O","D"}
-print(s1.difference(s2))
+country_code = {'India' : '0091',
+                'Australia' : '0025',
+                'Nepal' : '00977'}
+ 
+# search dictionary for country code of India
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+ 
+# search dictionary for country code of Japan
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
